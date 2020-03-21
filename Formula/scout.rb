05105d7e628fb0eb3b8cae22b294f4ae8dce8a7f
Alexis Bridoux <alexdroibux@gmail.com>
2020-03-21 12:31:39 +0100

    Download executable rather than build the program

--- Formula/scout.rb
+++ Formula/scout.rb
@@ -3,13 +3,14 @@ class Scout < Formula
   homepage "https://github.com/ABridoux/scout"
   url "https://github.com/ABridoux/scout/archive/0.2.0.tar.gz"
   sha256 "4f952ef20da86812f1520fb68bd6b1d03a6f98243d936b5a4d218685e214d910"
-  
-  depends_on :xcode => "10"
 
   def install
-    system "swift", "build", "-c", "release"
+    system "curl", "-o", "scout.zip", "https://abridoux-public.s3.us-east-2.amazonaws.com/scout/scout-latest.zip"
+    system "unzip", "scout.zip"
+    system "rm", "scout.zip"
     system "mkdir", "#{prefix}/bin"
-    system "install", ".build/release/ScoutCLT", "#{prefix}/bin/scout"
+    system "install", "scout", "#{prefix}/bin/scout"
+    system "rm", "scout"
   end
   
   test do
