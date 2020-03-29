ec95900eb65b5ab9319cd76a9bb47db3bf08f395
Alexis Bridoux <alexdroibux@gmail.com>
2020-03-29 11:54:49 +0200

    Updated to download latest scout version

--- Formula/scout.rb
+++ Formula/scout.rb
@@ -1,7 +1,7 @@
 class Scout < Formula
   desc "Make specific formats data values reading and writing simple when the data format is not known at build time."
   homepage "https://github.com/ABridoux/scout"
-  url "https://github.com/ABridoux/scout/releases/download/0.3.1/scout-0.3.1.zip"
+  url "https://github.com/ABridoux/scout/releases/download/0.3.1/scout.zip"
   sha256 "e80adcd8ec30a4583967bfaa535c96cf6081a2098aeeb606612aac4aaa4c9653"
 
   def install
