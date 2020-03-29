c835481c385ca8005ffa7288f61511c2d44625df
Alexis Bridoux <alexdroibux@gmail.com>
2020-03-29 11:35:53 +0200

    scout 0.3.1

--- Formula/scout.rb
+++ Formula/scout.rb
@@ -1,13 +1,10 @@
 class Scout < Formula
   desc "Make specific formats data values reading and writing simple when the data format is not known at build time."
   homepage "https://github.com/ABridoux/scout"
-  url "https://github.com/ABridoux/scout/archive/0.3.0.tar.gz"
-  sha256 "4e6662285c12d1f805cb2fccbc9f1aa4dfdc2b5dda4436c45594de687249b8b3"
+  url "https://github.com/ABridoux/scout/releases/download/0.3.1/scout-0.3.1.zip"
+  sha256 "e80adcd8ec30a4583967bfaa535c96cf6081a2098aeeb606612aac4aaa4c9653"
 
   def install
-    system "curl", "-o", "scout.zip", "https://abridoux-public.s3.us-east-2.amazonaws.com/scout/scout-latest.zip"
-    system "unzip", "scout.zip"
-    system "rm", "scout.zip"
     system "mkdir", "#{prefix}/bin"
     system "install", "scout", "#{prefix}/bin/scout"
     system "rm", "scout"
