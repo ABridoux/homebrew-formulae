79cfceda381696dc44f367570e27b91bee8b492f
Alexis Bridoux <alexdroibux@gmail.com>
2020-06-19 12:48:05 +0200

    lux 0.2.1

--- Formula/lux.rb
+++ Formula/lux.rb
@@ -1,8 +1,8 @@
 class Lux < Formula
   desc "Command-line tool to colorise plain code (e.g. for terminal output) or HTML files code blocks"
   homepage "https://github.com/ABridoux/lux"
-  url "https://github.com/ABridoux/lux/releases/download/0.2.0/lux.zip"
-  sha256 "73a33029400a3b66f1fcec405444b8351924f18fafd06f334664ec7701ac310b"
+  url "https://github.com/ABridoux/lux/releases/download/0.2.1/lux.zip"
+  sha256 "71eaeb04f2b67dbe8370105e2183c1167eb30312801e0c5c4de09ec95abf7868"
 
   def install
     system "mkdir", "#{prefix}/bin"
