86992786cbdbe29c65c74408dc494f8438c065c8
Alexis Bridoux <alexis1bridoux@gmail.com>
2021-04-08 15:55:37 +0200

    scout 3.0.3

--- Formula/scout.rb
+++ Formula/scout.rb
@@ -1,8 +1,8 @@
 class Scout < Formula
   desc "A command-line tool to read and modify Json, Plist, Yaml and Xml files using one single method."
   homepage "https://github.com/ABridoux/scout"
-  url "https://github.com/ABridoux/scout/releases/download/3.0.2/scout.zip"
-  sha256 "b126bf658a6bc9bb0ef3f64affa1e48d96608f3003d6eb3299ff4379fe127b4a"
+  url "https://github.com/ABridoux/scout/releases/download/3.0.3/scout.zip"
+  sha256 "299e10739d0def799f53d4891890322285355485a841af897a00e4ef1379ed69"
 
   def install
     system "mkdir", "#{prefix}/bin"
